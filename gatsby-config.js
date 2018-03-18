57af1716c6bcdbfbdfe48b67e9b2b54808f88646
Eneko <exocuted@gmail.com>
2018-03-18 08:20:57 +0100

    GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,13 @@ module.exports = {
   siteMetadata: {
     title: 'La Guinda',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet', {
+    resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-115952518-1",
+        // Puts tracking script in the head instead of the body
+        head: false
+      }
+  }],
   pathPrefix: "/laguinda"
 };
